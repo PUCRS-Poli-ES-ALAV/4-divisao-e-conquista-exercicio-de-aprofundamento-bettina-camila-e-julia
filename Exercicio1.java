b50c377d0d408e11e8f7b51161244bc8c58984db
juAlberti <julia.maia@acad.pucrs.br>
2022-03-16 14:27:40 -0300

    Exercicio 1 ok

---
+++ Exercicio1.java
@@ -0,0 +1,72 @@
+public class Exercicio1 {
+    public static void main(String args[])
+    {
+        int arr[] = { 5, 3, 3, 8, 1, 9, 2 };
+        
+        if (arr.length == 0){
+            for (int i = 0; i < arr.length; ++i)
+                System.out.print(arr[i] + " ");
+        } else {
+            Exercicio1 array = new Exercicio1();
+            array.sort(arr, 0, arr.length - 1);
+    
+            System.out.println("\n array merged");
+            int n = arr.length;
+            for (int i = 0; i < n; ++i)
+                System.out.print(arr[i] + " ");
+        }
+    }
+
+    void merge(int array[], int A, int middle, int B)
+    {
+        int n1 = middle - A + 1;
+        int n2 = B - middle;
+  
+        int Lft[] = new int[n1];
+        int Rght[] = new int[n2];
+  
+        for (int i = 0; i < n1; ++i)
+            Lft[i] = array[A + i];
+        for (int j = 0; j < n2; ++j)
+            Rght[j] = array[middle + 1 + j];
+
+        int i = 0, j = 0;
+  
+        int k = A;
+        while (i < n1 && j < n2) {
+            if (Lft[i] <= Rght[j]) {
+                array[k] = Lft[i];
+                i++;
+            }
+            else {
+                array[k] = Rght[j];
+                j++;
+            }
+            k++;
+        }
+  
+        while (i < n1) {
+            array[k] = Lft[i];
+            i++;
+            k++;
+        }
+  
+        while (j < n2) {
+            array[k] = Rght[j];
+            j++;
+            k++;
+        }
+    }
+  
+    void sort(int array[], int left, int right)
+    {
+        if (left < right) {
+            int x =left+ (right-left)/2;
+  
+            sort(array, left, x);
+            sort(array, x + 1, right);
+  
+            merge(array, left, x, right);
+        }
+    }
+}
