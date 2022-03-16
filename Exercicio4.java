b38d2a3554f4e8619145f7e950b08b9645d907fb
Camila-2120 <Camila.Maccari@edu.pucrs.br>
2022-03-16 19:56:27 -0300

    Adicionando o exercicio 4

--- Exercicio4.java
+++ Exercicio4.java
@@ -2,29 +2,29 @@
 //Nestes testes, contabilize o número de iterações que o algoritmo executa, e o tempo gasto.
 import java.math.*;
 
-public class Exercicio4 {// E Exercicio5 tbm
+public class Exercicio4 {
         public static void main(String args[]) {
-            long a = 1.0;
-            long b = 1.0;
-            long n = 0.0;
+            double a = 3.456;
+            double b = 3.456;
+            double n = 64;
             Exercicio4 exer = new Exercicio4();
-            exer.MULTIPLY(a, b, n);
+            System.out.println(exer.MULTIPLY(a, b, n));
         }
     
-    long MULTIPLY(long x,long y,long n) {  
-        // if (n == 1){
-        //     return x * y;
-        // } else {
-        // long m = Math.ceil(n / 2);
-        // long a = Math.floor(x / Math.pow(2, m)); 
-        // long b = x % Math.pow(2, m);
-        // long c = Math.floor(y / Math.pow(2, m)); 
-        // long d = y % Math.pow(2, m);
-        // long e = MULTIPLY(a, c, m);
-        // long f = MULTIPLY(b, d, m);
-        // long g = MULTIPLY(b, c, m);
-        // long h = MULTIPLY(a, d, m);
-        // return Math.pow(2, (2*m))*e + Math.pow(2, m)*(g + h) + f;
-        // }
+    double MULTIPLY(double x,double y,double n) {  
+         if (n == 1){
+             return x * y;
+         } else {
+         double m = Math.ceil(n / 2);
+         double a = Math.floor(x / Math.pow(2, m)); 
+         double b = x % Math.pow(2, m);
+         double c = Math.floor(y / Math.pow(2, m)); 
+         double d = y % Math.pow(2, m);
+         double e = MULTIPLY(a, c, m);
+         double f = MULTIPLY(b, d, m);
+         double g = MULTIPLY(b, c, m);
+         double h = MULTIPLY(a, d, m);
+         return Math.pow(2, (2*m))*e + Math.pow(2, m)*(g + h) + f;
+         }
     }
 }
