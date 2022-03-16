1125bc0cbca289d5a9a443f90f5586e55d720cfd
juAlberti <julia.maia@acad.pucrs.br>
2022-03-16 20:26:33 -0300

    Exercicio 4 falta os contabilizar direito as iteracoes

--- Exercicio4.java
+++ Exercicio4.java
@@ -4,27 +4,30 @@ import java.math.*;
 
 public class Exercicio4 {
         public static void main(String args[]) {
-            double a = 3.456;
-            double b = 3.456;
-            double n = 64;
+            long a = 1500;
+            long b = 1000;
+            long n = 64;
             Exercicio4 exer = new Exercicio4();
             System.out.println(exer.MULTIPLY(a, b, n));
         }
     
-    double MULTIPLY(double x,double y,double n) {  
-         if (n == 1){
-             return x * y;
-         } else {
-         double m = Math.ceil(n / 2);
-         double a = Math.floor(x / Math.pow(2, m)); 
-         double b = x % Math.pow(2, m);
-         double c = Math.floor(y / Math.pow(2, m)); 
-         double d = y % Math.pow(2, m);
-         double e = MULTIPLY(a, c, m);
-         double f = MULTIPLY(b, d, m);
-         double g = MULTIPLY(b, c, m);
-         double h = MULTIPLY(a, d, m);
-         return Math.pow(2, (2*m))*e + Math.pow(2, m)*(g + h) + f;
-         }
+    long MULTIPLY(long x,long y,long n) {  
+        int count = 0;
+        if (n == 1){
+            count = count + 1;
+            return x * y;
+        } else {
+        long m = n / 2;
+        long a = x / (long)Math.pow(2, m); 
+        long b = x % (long)Math.pow(2, m);
+        long c = y / (long)Math.pow(2, m); 
+        long d = y % (long)Math.pow(2, m);
+        long e = MULTIPLY(a, c, m);
+        long f = MULTIPLY(b, d, m);
+        long g = MULTIPLY(b, c, m);
+        long h = MULTIPLY(a, d, m);
+        
+        return ((long)Math.pow(2, (2*m)))*e + ((long)Math.pow(2, m)*(g + h) + f);
+        }
     }
 }
